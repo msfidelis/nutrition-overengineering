530869fe65f79c7a44b96058254d2f42020c03ab
Matheus Fidelis <matheus@nanoshots.com.br>
2025-05-02 14:20:41 -0300

    Otel: Trace ID Print in Logs

--- health-api/pkg/message/main.go
+++ health-api/pkg/message/main.go
@@ -3,20 +3,14 @@ package message
 import (
 	"context"
 	"os"
-
-	"go.opentelemetry.io/otel/trace"
 )
 
 func SendMessage(ctx context.Context, message string) {
 
-	span := trace.SpanFromContext(ctx)
-	span.SetName("Define Message Type")
-	defer span.End()
-
 	// send message
 	switch os.Getenv("MESSAGE_TYPE") {
 	case "sqs":
-		SendSQSMessage(trace.ContextWithSpan(ctx, span), message)
+		SendSQSMessage(ctx, message)
 		return
 	case "kafka":
 		return
